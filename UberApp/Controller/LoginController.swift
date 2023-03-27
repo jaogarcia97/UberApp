d71483e57136384f11d18410d69cec4ea0c22f8e
Jao Garcia <josejaimefelixgarcia@gmail.com>
2023-03-27 15:38:34 +0800

    Login UI with email, password, title, loginbutton, signup button

--- UberApp/Controller/LoginController.swift
+++ UberApp/Controller/LoginController.swift
@@ -48,9 +48,25 @@ class LoginController:UIViewController {
     private let loginUIButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Login", for: .normal)
-        button.setTitleColor(UIColor(white: 1, alpha: 0.5), for: .normal)
-        button.backgroundColor = .blue
+        button.setTitleColor(UIColor(white: 1, alpha: 0.8), for: .normal)
+        button.backgroundColor = .mainBlueTint //Custom Color
+        button.layer.cornerRadius = 5
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        //Make Mutable Attributed String
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ",
+                                                        attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+                                                                     NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint]))
+        
+        //button.addTarget(self, action: #selector(handleShowSignUp), for: (.touchUpInside)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
         return button
     }()
     
@@ -62,8 +78,7 @@ class LoginController:UIViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
-       
+        view.backgroundColor = .backgroundColor //Custom App BG Color
         //Initialize Title label
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
@@ -85,6 +100,10 @@ class LoginController:UIViewController {
 //        //Initialize Password Subview
 //        view.addSubview(passwordContainerView)
 //        passwordContainerView.anchor(top: emailContainerView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16,height: 50)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(bottom:view.safeAreaLayoutGuide.bottomAnchor, height:32)
+        dontHaveAccountButton.centerX(inView: view)
         
     
     }
